3aa15a4c8e274789e9759b732552a3a2161e1fcd
g.tsenis@gmail.com <g.tsenis@gmail.com>
2019-10-25 22:53:58 +0300

    Moved Portal

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : Collidable {
+	public string[] sceneNames;
+
+	protected override void OnCollide(Collider2D coll) {
+		if (coll.name == "Player") {
+			GameManager.instance.SaveState();
+			string sceneName = sceneNames[Random.Range(0, sceneNames.Length)];
+			UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName);
+		}
+	}
+}
