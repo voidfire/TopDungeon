ebc725aaebd9c612ee9c267b428f27cc9bd7ae6f
g.tsenis@gmail.com <g.tsenis@gmail.com>
2019-10-25 23:24:31 +0300

    Floating Text Manager

---
+++ Assets/Scripts/FloatingTextManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloatingTextManager : MonoBehaviour {
+
+	public GameObject textContainer;
+	public GameObject textPrefab;
+
+	private List<FloatingText> floatingTexts = new List<FloatingText>();
+
+	private FloatingText GetFloatingText(){
+		FloatingText txt = floatingTexts.Find(t => !t.active);
+
+		if (txt == null) {
+			txt = new FloatingText();
+			txt.go = Instantiate(textPrefab);
+			txt.go.transform.SetParent(textContainer.transform);
+			txt.txt = txt.go.GetComponent<Text>();
+
+			floatingTexts.Add(txt);
+		}
+
+		return txt;
+	}
+
+}
