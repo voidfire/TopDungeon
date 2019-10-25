9113380900a79c4ea961befc725c3b85627b567f
g.tsenis@gmail.com <g.tsenis@gmail.com>
2019-10-25 23:34:20 +0300

    Floating Text show function

--- Assets/Scripts/FloatingTextManager.cs
+++ Assets/Scripts/FloatingTextManager.cs
@@ -7,8 +7,23 @@ public class FloatingTextManager : MonoBehaviour {
 
 	public GameObject textContainer;
 	public GameObject textPrefab;
-
 	private List<FloatingText> floatingTexts = new List<FloatingText>();
+
+	private void Update() {
+		foreach (FloatingText txt in floatingTexts)
+			txt.UpdateFloatingText();
+	}
+	
+	public void Show(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration) {
+		FloatingText floatingText = GetFloatingText();
+		floatingText.txt.text = msg;
+		floatingText.txt.fontSize = fontSize;
+		floatingText.txt.color = color;
+		floatingText.txt.transform.position = Camera.main.WorldToScreenPoint(position); // Transfer world spavce to screen space so we can use it in UI
+		floatingText.motion = motion;
+		floatingText.duration = duration;
+		floatingText.Hide();
+	}
 
 	private FloatingText GetFloatingText(){
 		FloatingText txt = floatingTexts.Find(t => !t.active);
@@ -18,7 +33,6 @@ public class FloatingTextManager : MonoBehaviour {
 			txt.go = Instantiate(textPrefab);
 			txt.go.transform.SetParent(textContainer.transform);
 			txt.txt = txt.go.GetComponent<Text>();
-
 			floatingTexts.Add(txt);
 		}
 
